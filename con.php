21592ecee7a3895dcc9c936061eee3b9b268510f
Qasim345 <69038179+Qasim345@users.noreply.github.com>
2021-11-23 19:17:04 +0430

    Update con.php

--- con.php
+++ con.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("sql310.epizy.com", "epiz_30432254", "qasimtesthost", "epiz_30432254_todo_db") or die("Not Connected!");
+$con = mysqli_connect("sql310.epizy.com", "epiz_30432254", "NC0T9pHBDN", "epiz_30432254_todo_db") or die("Not Connected!");
 ?>
